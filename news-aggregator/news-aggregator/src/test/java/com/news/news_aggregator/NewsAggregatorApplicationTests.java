8247449dfe4ad022660de6802d51ce2fbf91c210
Shashank Pratap Singh <148487590+shashankps29@users.noreply.github.com>
2025-09-07 14:39:59 +0530

    Add files via upload

---
+++ news-aggregator/news-aggregator/src/test/java/com/news/news_aggregator/NewsAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.news_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
